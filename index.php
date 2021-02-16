e2a9c7d96126ee3956627c854b89945c7d7f803f
Jean tsheta <tshieta@gmail.com>
2021-02-16 19:18:11 +0200

    encapsulation.

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
 // PHP assessment 
+
+class ImageResize
+{
+ public $img_type;
+
+ protected $source_image;
+
+ protected $original_image_width; 
+ protected $original_image_height; 
+
+ 
+protected $dest_image_width;
+protected $dest_image_height;
+
+public function __construct($filename){
+
+ 
+}
+
+
+}
+
 ?>
